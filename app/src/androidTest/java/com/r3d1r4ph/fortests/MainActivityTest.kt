65c1eea3d7c2b23e7b40bdbdf6fb2dce41c12293
Armen Gevorkyan <rediraph@mail.ru>
2022-04-12 17:12:57 +0700

    added 2e2 test

---
+++ app/src/androidTest/java/com/r3d1r4ph/fortests/MainActivityTest.kt
@@ -0,0 +1,36 @@
+package com.r3d1r4ph.fortests
+
+import androidx.test.espresso.Espresso
+import androidx.test.espresso.action.ViewActions
+import androidx.test.espresso.assertion.ViewAssertions
+import androidx.test.espresso.matcher.ViewMatchers
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import junit.framework.TestCase
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class MainActivityTest : TestCase() {
+
+    @get:Rule
+    var activityRule: ActivityScenarioRule<MainActivity> =
+        ActivityScenarioRule(MainActivity::class.java)
+
+    @Test
+    fun inputDataAndGiveCorrectAnswerActivity() {
+        Espresso.onView(ViewMatchers.withId(R.id.tillsCountTextInputEditText))
+            .perform(ViewActions.typeText("1"), ViewActions.closeSoftKeyboard())
+
+        Espresso.onView(ViewMatchers.withId(R.id.customersTimeTextInputEditText)).perform(
+            ViewActions.typeText("4"),
+            ViewActions.closeSoftKeyboard()
+        )
+
+        Espresso.onView(ViewMatchers.withId(R.id.sendButton)).perform(ViewActions.click())
+
+        Espresso.onView(ViewMatchers.withId(R.id.answerTextView))
+            .check(ViewAssertions.matches(ViewMatchers.withText("Answer: 4")))
+    }
+}
--- app/src/main/java/com/r3d1r4ph/fortests/MainActivity.kt
+++ app/src/main/java/com/r3d1r4ph/fortests/MainActivity.kt
@@ -2,11 +2,34 @@ package com.r3d1r4ph.fortests
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.r3d1r4ph.fortests.exception.NotPositiveIntegerException
+import by.kirich1409.viewbindingdelegate.viewBinding
+import com.r3d1r4ph.fortests.databinding.ActivityMainBinding
+import com.r3d1r4ph.fortests.model.TheSupermarketQueue
 
 class MainActivity : AppCompatActivity() {
+
+    private val viewBinding by viewBinding(ActivityMainBinding::bind, R.id.rootLayout)
+    private val theSupermarketQueue = TheSupermarketQueue()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        with(viewBinding) {
+            answerTextView.text = getString(
+                R.string.answer, 0
+            )
+
+            sendButton.setOnClickListener {
+                answerTextView.text = getString(
+                    R.string.answer, theSupermarketQueue.calculateTotalTimeOfQueues(
+                        customers = customersTimeTextInputEditText.text.toString().split(" ")
+                            .map { it.toInt() }.toIntArray(),
+                        tillsCount = tillsCountTextInputEditText.text.toString().toInt()
+                    )
+                )
+            }
+
+        }
     }
 }
